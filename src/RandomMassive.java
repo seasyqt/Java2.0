a6d0e95c9e64edb9ea6cce7ae693f7449d472040
seasyqt <kira_man@mail.ru>
2019-10-13 11:22:13 +0300

    Исправление в коде , изменена перестановка макс и мин значений местами в массиве

--- src/RandomMassive.java
+++ src/RandomMassive.java
@@ -2,13 +2,14 @@ public class RandomMassive {
     public static void main(String[] args) {
         int [] randomMassive = new int[20]; // объявление переменной массива c 20 ячейками
         //цикл с рандомным заполнением ячеек массива
-        int numberMax = 0;
+
         for (int numberElementsMassive = 0;numberElementsMassive < randomMassive.length; numberElementsMassive++)
         {
             randomMassive[numberElementsMassive] = (int)Math.round(-10+ (Math.random() * 20)); // пристоение элементу массива случайное число
             System.out.print(randomMassive[numberElementsMassive] + " "); //вывод всех рандомных чисел
         }
         int max = randomMassive[0] , min = randomMassive[0]; // объявление макс и мин
+int numberMax = 0,numberMin = 0;
 
         for(int numberElementsMassive = 0;numberElementsMassive < randomMassive.length;numberElementsMassive++)
         {
@@ -20,16 +21,17 @@ public class RandomMassive {
             if (randomMassive[numberElementsMassive] < min) //тоже самое ,что и выше только с мин.числом
             {
                 min = randomMassive[numberElementsMassive];
+                numberMin = numberElementsMassive;
             }
         }
         System.out.println("\nМаксимальное число " + max); //вывод макс числа
         System.out.println("Минимальное число " + min);  //вывод мин числа
 
-        int temp = max; // замена мин и макс через 3ю переменную
-        max = min;
-        min = temp;
-        System.out.println("Теперь максимальное число такое:" + max); // вывод макс после изменения
-        System.out.println("Теперь минимальное число такое:" + min); // вывод мин после изменения
+        //замена значения в массиве
+        randomMassive[numberMax] = min;
+        randomMassive[numberMin] = max;
+        System.out.println("Теперь максимальное число такое:" + randomMassive[numberMax]); // вывод макс после изменения
+        System.out.println("Теперь минимальное число такое:" + randomMassive[numberMin]); // вывод мин после изменения
 
     }
 }
