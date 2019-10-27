ad5ba49c4bf1f0d405e4b8c22c0bf7ab224d1da2
seasyqt <kira_man@mail.ru>
2019-10-27 10:36:55 +0300

    Добвление для класса конструкции try/catch - деление на 0, неверный ввод чисел и NullPointerException

--- src/calculator/Main.java
+++ src/calculator/Main.java
@@ -1,23 +1,34 @@
 package calculator;
 
+import java.util.Arrays;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
+        try {
         Scanner scanner = new Scanner(System.in);
         Calculator first = new Calculator();
         System.out.println("Вы запустили калькулятор");
         System.out.println("Введите первое число : ");
-    first.setFirstNumber(scanner.nextDouble());
-        System.out.println("Введите второе число : ");
-    first.setSecondNumber(scanner.nextDouble());
-        System.out.println("Введите тип операции над числами из \"+,-,/,*\"");
-    first.setOperation(scanner.next().charAt(0));
-        first.result();
-        System.out.println("Результат : " + first.getResult());
-
-
+            first.setFirstNumber(scanner.nextDouble());
+            System.out.println("Введите второе число : ");
+            first.setSecondNumber(scanner.nextDouble());
+            System.out.println("Введите тип операции над числами из \"+,-,/,*\"");
+            first.setOperation(scanner.next().charAt(0));
+            first.result();
+            System.out.println("Результат : " + first.getResult());
+        }catch (InputMismatchException e ) {
+            System.out.println("Некорректный ввод числа");
+            e.printStackTrace();
+        }catch (NullPointerException e) {
+            System.out.println("Ссылка для объекта равна null");
+            e.printStackTrace();
+        }catch (ArithmeticException e){
+            System.out.println("Ошибка выполнения , деление на ноль");
+            e.printStackTrace();
+        }
     }
 
 }
